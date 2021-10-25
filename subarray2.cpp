bbd8a4411a0dba0098b9a48139d5db89d50cca4e
lakshya550 <93146326+lakshya550@users.noreply.github.com>
2021-10-25 21:26:34 +0530

    subarray problem

---
+++ subarray2.cpp
@@ -0,0 +1,25 @@
+//CODE TO PRINT THE SUM OF ELEMENTS OF ALL POSSIBLE SUBARRAYS
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++) 
+    cin>>a[i];
+
+    int sum=0;
+    for (int i = 0; i < n; i++)
+    {
+        sum=0;
+        for(int j=i; j<n; j++){
+        sum+=a[j];
+        cout<<sum<<"\n";
+        }
+    }
+    
+       return 0;
+}
