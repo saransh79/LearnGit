f284e8711b9001af4e2ab536f278fb358e0e96ad
Ojas Jaiswal <114718987+OjasKr@users.noreply.github.com>
2022-10-13 19:10:32 +0530

    Update selection_sort.c
    declared loop variables i and j at the beginning with other variables.
    replaced int return type with void.

--- Sorting Programs/selection_sort.c
+++ Sorting Programs/selection_sort.c
@@ -3,11 +3,11 @@
 // SELECTION SORT
 
 void selection_sort(int arr[], int n) {
-	int min_idx, temp;
-	for (int i = 0; i < n - 1; i++)
+	int min_idx, temp, i, j;
+	for (i = 0; i < n - 1; i++)
 	{
 		min_idx = i;
-		for (int j = i + 1; j < n; j++) {
+		for (j = i + 1; j < n; j++) {
 			if (arr[j] < arr[min_idx])
 				min_idx = j;
 		}
@@ -27,13 +27,11 @@ void print_arr(int arr[], int n) {
 		printf("%d ", arr[i]);
 }
 
-int main()
+void main()
 {
 	int arr[] = { 80, 90, 100, 40, 50, 30, 20, 70, 60, 10}, n = 10;
 
 	selection_sort(arr, n);
 
 	print_arr(arr, n);
-
-	return 0;
 }
